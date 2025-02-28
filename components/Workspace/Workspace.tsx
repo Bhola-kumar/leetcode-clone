b4dedb835746ba4c2b24ff525170e80f14906f40
Bhola-straive <Bhola.Kumar@straive.com>
2025-02-28 18:37:57 +0530

    working on home page

--- components/Workspace/Workspace.tsx
+++ components/Workspace/Workspace.tsx
@@ -1,9 +1,24 @@
+"use client";
 import React from "react";
-import Split from "react-split";
+import ProblemDescription from "./ProblemDescription/ProblemDescription";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Playground from "./Playground/Playground";
+
 const Workspace: React.FC = () => {
     return (
-        <>
-        </>
+        <div className="flex flex-1 h-[calc(100vh-50px)]">
+            <PanelGroup direction="horizontal" className="w-full">
+                <Panel defaultSize={50} minSize={0} >
+                    <ProblemDescription />  
+                </Panel>
+
+                <PanelResizeHandle />
+
+                <Panel defaultSize={50} minSize={0}>
+                    <Playground />
+                </Panel>
+            </PanelGroup>
+        </div>
     );
 };
 
